8539071e76730bde4399441ff0162ab1f931f308
mbathaedward <mbathaedward11@gmail.com>
2024-07-04 09:39:31 +0300

    Program a calcultor

---
+++ asig.py
@@ -0,0 +1,23 @@
+#A program to perform mathematics calculations.
+#getting user inputs from the user
+
+number1 = int(input("enter the firstnumber:"))
+number2 = int(input("enter the secondnumber:"))
+
+# declaration  of variables
+addition = number1 + number2
+division = number1 / number2
+Multiplication = number1 * number2
+Subtraction = number1 - number2
+
+#getting user outputs on console
+addition = (f"the addition of {number1} and {number2} is: {addition}")
+division = (f"the division of {number1 } and {number2} is:{division}")
+Multiplication =(f"the multiplication of {number1} and {number2 } is:{Multiplication}")
+Subtraction = (f"the subtraction of {number1} and {number2} is: {Subtraction}")
+
+print(addition)
+print(division)
+print(Multiplication)
+print(Subtraction)
+
